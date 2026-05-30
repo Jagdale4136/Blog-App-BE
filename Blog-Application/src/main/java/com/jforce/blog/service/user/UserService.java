d863465cb60672ff18c0c91b1331dbc754c3d818
Jagdale Kiran <119597791+Jagdale4136@users.noreply.github.com>
2026-05-30 16:53:01 +0530

    Update UserService.java

--- Blog-Application/src/main/java/com/jforce/blog/service/user/UserService.java
+++ Blog-Application/src/main/java/com/jforce/blog/service/user/UserService.java
@@ -1,8 +1,11 @@
 package com.jforce.blog.service.user;
 
+import com.jforce.blog.dto.AuthResponse;
 import com.jforce.blog.dto.UserDTO;
 
 public interface UserService {
 
     UserDTO createNewUser(UserDTO userDTO);
+
+    AuthResponse signIn(UserDTO userDTO);
 }
